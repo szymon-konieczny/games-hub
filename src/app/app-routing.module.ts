0cfe1ff9e31998ef674768a3dd82c2400299bd3d
szymon-konieczny <szykon@gmail.com>
2020-10-24 19:32:16 +0200

    [GH-3] Remove home page link button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './main/main.component';
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
